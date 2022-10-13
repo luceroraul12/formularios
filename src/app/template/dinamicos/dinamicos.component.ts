f8a62dbc3005393d6900dbf9cf7d4c3b960c23cd
Lucero Raul <luceroraul12@gmail.com>
2022-10-13 15:38:34 -0300

    add inputs dinamicos

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Persona {
+  nombre: string;
+  juegosFavoritos: Juego[];
+}
+
+interface Juego {
+  id: number;
+  nombre: string;
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
@@ -8,6 +18,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicosComponent implements OnInit {
 
+  public persona: Persona = {
+    nombre: "Raul",
+    juegosFavoritos: [
+      {
+        id: 2435,
+        nombre: "Mario Kart"
+      },
+      {
+        id: 25346,
+        nombre: "Overwatch"
+      }
+    ]
+  }
+
+  // para crear inputs dinamico, hay que crear interfaces, y la encargada del input dinamico(ngfor) debe ser un arreglo y que cada uno tenga name diferente ejemplo variabl_concatenacion
   constructor() { }
 
   ngOnInit(): void {
@@ -15,6 +40,10 @@ export class DinamicosComponent implements OnInit {
 
   guardar(): void{
     alert('formulario cargado')
+  }
+
+  agregarJuego(){
+    this.persona.juegosFavoritos.push()
   }
 
 }
